7f68369734fa874bd68c983f275dca2b0fd4facf
Ramu Bhukya <ramubhukya@lavar.local>
2025-05-11 01:15:32 +0530

    App js file change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          My Pfole loading ...
+          My Profile loading ...
         </p>
       </header>
     </div>
