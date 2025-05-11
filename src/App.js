2d6568c78a546f6b3960e1918e711a2cf562af85
Ramu Bhukya <ramubhukya@lavar.local>
2025-05-11 22:11:18 +0530

    Adding Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          My Profile loading ...
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <h2>About Page</h2>
+            <p>This is the about page.</p>
+          </Route>
+          <Route path="/contact">
+            <h2>Contact Page</h2>
+            <p>This is the contact page.</p>
+          </Route>
+          <Route path="/">
+            <h2>Home Page</h2>
+            <p>Welcome to the home page!</p>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
