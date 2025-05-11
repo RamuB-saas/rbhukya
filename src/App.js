e65e1b3d90657bed53f644e25eea325dac5b3bce
Ramu Bhukya <ramubhukya@lavar.local>
2025-05-11 22:49:17 +0530

    App js changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/pages/about">About</Link>
             </li>
           </ul>
         </nav>
